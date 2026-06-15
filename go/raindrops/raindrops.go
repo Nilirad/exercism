76a301f26c1a7d9be9500d79283d85c9131fc34c
Nilirad <gisquerin@gmail.com>
2026-06-15 18:06:09 +0200

    Complete raindrops exercise

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,26 @@
 package raindrops
 
+import "strconv"
+
 func Convert(number int) string {
-	panic("Please implement the Convert function")
+	var out string
+
+	if number%3 == 0 {
+		out += "Pling"
+	}
+
+	if number%5 == 0 {
+		out += "Plang"
+	}
+
+	if number%7 == 0 {
+		out += "Plong"
+	}
+
+	if len(out) == 0 {
+		return strconv.Itoa(number)
+	} else {
+		return out
+	}
+
 }
