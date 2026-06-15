b9546d4ec03cf021aa4a3d8c922bc98131ac5070
Nilirad <gisquerin@gmail.com>
2026-06-15 17:03:44 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Go environment for the Go Exercism track";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs supportedSystems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            exercism
+            gopls           # The official Go language server
+            gotools         # Essential utilities (like goimports)
+            golangci-lint   # The paradigmatic linter for Go codebases
+          ];
+
+          shellHook = ''
+            echo "🚀 Exercism Go Workspace Synchronized"
+            echo "Using Go: $(go version)"
+            echo ""
+            echo "If this is your inaugural run, remember to authenticate the CLI:"
+            echo "  exercism configure --token=YOUR_API_TOKEN"
+            echo ""
+          '';
+        };
+      });
+    };
+}
