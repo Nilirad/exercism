a9b6cc24c2d9662e9dc6ca23974282f7d5c9b726
Nilirad <gisquerin@gmail.com>
2026-06-17 19:39:35 +0200

    Complete Go `collatz-conjecture`

--- go/collatz-conjecture/collatz_conjecture.go
+++ go/collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,28 @@
 package collatzconjecture
 
+import (
+	"errors"
+	"math"
+)
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n < 1 {
+		return 0, errors.New("`n` must be greater than zero.")
+	}
+
+	steps := 0
+
+	for n > 1 {
+		if n%2 == 0 {
+			n /= 2
+		} else {
+			if n >= (math.MaxInt-1)/3 {
+				return 0, errors.New("Integer overflow")
+			}
+			n = n*3 + 1
+		}
+		steps++
+	}
+
+	return steps, nil
 }
