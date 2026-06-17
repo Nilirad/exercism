f77812b1c8e12edfa3510a486db99eb1a27df3a4
Nilirad <gisquerin@gmail.com>
2026-06-17 18:15:54 +0200

    Complete Go `weather-forecast`

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,10 +1,15 @@
+// Package weather provides tools for weather forecasting.
 package weather
 
 var (
+	// CurrentCondition represents the current weather condition.
 	CurrentCondition string
-	CurrentLocation  string
+
+	// CurrentLocation represents the current location.
+	CurrentLocation string
 )
 
+// Forecast shows the weather forecast for a city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
