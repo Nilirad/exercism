cc90fe3c1a49cf6e9bc6dc33af7d93c68241f9ba
Nilirad <gisquerin@gmail.com>
2026-06-18 17:04:53 +0200

    Complete Go `darts`

--- go/darts/darts.go
+++ go/darts/darts.go
@@ -1,5 +1,28 @@
 package darts
 
+import "math"
+
+const (
+	smallCircle        = 1.0
+	mediumCircle       = 5.0
+	bigCircle          = 10.0
+	smallCirclePoints  = 10
+	mediumCirclePoints = 5
+	bigCirclePoints    = 1
+	missPoints         = 0
+)
+
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	distance := math.Sqrt(x*x + y*y)
+
+	if distance <= smallCircle {
+		return smallCirclePoints
+	}
+	if distance <= mediumCircle {
+		return mediumCirclePoints
+	}
+	if distance <= bigCircle {
+		return bigCirclePoints
+	}
+	return missPoints
 }
