088310bb9b87e5b615d7490833c485aed8e97463
Nilirad <gisquerin@gmail.com>
2026-06-19 13:07:02 +0200

    Complete Go `isbn-verifier`

--- go/isbn-verifier/isbn_verifier.go
+++ go/isbn-verifier/isbn_verifier.go
@@ -1,5 +1,28 @@
 package isbnverifier
 
 func IsValidISBN(isbn string) bool {
-	panic("Please implement the IsValidISBN function")
+	multiplier := uint(10)
+	accumulator := uint(0)
+	for i := range len(isbn) {
+		char := isbn[i]
+		switch {
+		case char == '-':
+			continue
+		case char == 'X':
+			if multiplier != 1 {
+				return false
+			}
+			accumulator += 10
+			multiplier--
+		case char >= '0' && char <= '9':
+			digit := uint(char - '0')
+			accumulator += digit * multiplier
+			multiplier--
+		default:
+			return false
+		}
+	}
+
+	numDigitsOk := multiplier == 0
+	return numDigitsOk && accumulator%11 == 0
 }
