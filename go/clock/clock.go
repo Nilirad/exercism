d104c2e7c232afb1c00e46fb284a46c639126e70
Nilirad <gisquerin@gmail.com>
2026-06-21 19:10:05 +0200

    Complete `Go` clock

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,19 +1,33 @@
 package clock
 
-// Define the Clock type here.
+import "fmt"
+
+type Clock struct {
+	h int
+	m int
+}
+
+const minutesPerDay = 60 * 24
 
 func New(h, m int) Clock {
-	panic("Please implement the New function")
+	minutesTotal := h*60 + m
+	minutesInDay := (minutesTotal%minutesPerDay + minutesPerDay) % minutesPerDay
+
+	hours := minutesInDay / 60
+	minutes := minutesInDay - 60*hours
+	return Clock{h: hours, m: minutes}
 }
 
 func (c Clock) Add(m int) Clock {
-	panic("Please implement the Add function")
+	minutesTotal := c.h*60 + c.m + m
+	return New(0, minutesTotal)
 }
 
 func (c Clock) Subtract(m int) Clock {
-	panic("Please implement the Subtract function")
+	minutesTotal := c.h*60 + c.m - m
+	return New(0, minutesTotal)
 }
 
 func (c Clock) String() string {
-	panic("Please implement the String function")
+	return fmt.Sprintf("%02d:%02d", c.h, c.m)
 }
