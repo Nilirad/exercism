616f59a15e8861acfd61cc574fc17fc7b5e07e76
Nilirad <gisquerin@gmail.com>
2026-06-21 19:28:41 +0200

    Update Go `clock`

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -3,31 +3,29 @@ package clock
 import "fmt"
 
 type Clock struct {
-	h int
-	m int
+	minutes int
 }
 
 const minutesPerDay = 60 * 24
 
 func New(h, m int) Clock {
-	minutesTotal := h*60 + m
-	minutesInDay := (minutesTotal%minutesPerDay + minutesPerDay) % minutesPerDay
-
-	hours := minutesInDay / 60
-	minutes := minutesInDay - 60*hours
-	return Clock{h: hours, m: minutes}
+	minutesModulo := ((h*60+m)%minutesPerDay + minutesPerDay) % minutesPerDay
+	return Clock{minutes: minutesModulo}
 }
 
 func (c Clock) Add(m int) Clock {
-	minutesTotal := c.h*60 + c.m + m
-	return New(0, minutesTotal)
+	minutes := c.minutes + m
+	return New(0, minutes)
 }
 
 func (c Clock) Subtract(m int) Clock {
-	minutesTotal := c.h*60 + c.m - m
-	return New(0, minutesTotal)
+	minutes := c.minutes - m
+	return New(0, minutes)
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.h, c.m)
+	hours := c.minutes / 60
+	minutes := c.minutes - 60*hours
+
+	return fmt.Sprintf("%02d:%02d", hours, minutes)
 }
