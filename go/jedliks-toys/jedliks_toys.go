ba97832798d69a7a6bb0a8172f874ee7bb365445
Nilirad <gisquerin@gmail.com>
2026-06-30 17:35:25 +0200

    Complete Go `jedliks-toys`

--- go/jedliks-toys/jedliks_toys.go
+++ go/jedliks-toys/jedliks_toys.go
@@ -1,18 +1,41 @@
 package jedlik
 
-// TODO: define the 'Drive()' method
+import "fmt"
 
-// TODO: define the 'DisplayDistance() string' method
+type Track struct {
+	distance int
+}
 
-// TODO: define the 'DisplayBattery() string' method
+// NewTrack creates a new track
+func NewTrack(distance int) Track {
+	return Track{
+		distance: distance,
+	}
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+// Drive drives the car one time. If there is not enough battery to drive one more time,
+// the car will not move.
+func (car *Car) Drive() {
+	finalBattery := car.battery - car.batteryDrain
+	if finalBattery < 0 {
+		return
+	}
 
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
+	car.distance += car.speed
+	car.battery = finalBattery
+}
+
+func (car Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", car.distance)
+}
+
+func (car Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", car.battery)
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func (car Car) CanFinish(distance int) bool {
+	drain := distance * car.batteryDrain / car.speed
+	finalBattery := car.battery - drain
+	return finalBattery >= 0
+}
