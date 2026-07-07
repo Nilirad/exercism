36dfa1778862a55c8c3b689d7688d27d5417e0de
Nilirad <gisquerin@gmail.com>
2026-07-07 17:43:58 +0200

    Complete Go `pangram`

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,34 @@
 package pangram
 
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	const lettersInAlphabet = 26
+	if len(input) < lettersInAlphabet {
+		return false
+	}
+
+	var seen [lettersInAlphabet]bool
+
+	// We can iterate over bytes instead of runes
+	// because we are searching English letters which are all ASCII characters.
+	for i := range len(input) {
+
+		char := input[i]
+		if char >= 'A' && char <= 'Z' {
+			char += 'a' - 'A'
+		}
+		if char < 'a' || char > 'z' {
+			continue
+		}
+
+		index := int(char - 'a')
+		seen[index] = true
+	}
+
+	for i := range lettersInAlphabet {
+		if !seen[i] {
+			return false
+		}
+	}
+
+	return true
 }
