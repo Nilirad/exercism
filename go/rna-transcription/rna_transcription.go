ca58ec7f73915363f381548806a3613a440b2ad8
Nilirad <gisquerin@gmail.com>
2026-07-08 15:14:56 +0200

    Complete Go `rna-transcription`

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,5 +1,22 @@
 package rnatranscription
 
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	rna := make([]byte, len(dna))
+
+	for i := range len(dna) {
+		switch dna[i] {
+		case 'G':
+			rna[i] = 'C'
+		case 'C':
+			rna[i] = 'G'
+		case 'T':
+			rna[i] = 'A'
+		case 'A':
+			rna[i] = 'U'
+		default:
+			return ""
+		}
+	}
+
+	return string(rna)
 }
