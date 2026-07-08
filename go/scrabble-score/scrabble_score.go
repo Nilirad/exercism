6b718e9475731a13145b4841f4405d05596d3049
Nilirad <gisquerin@gmail.com>
2026-07-08 15:33:45 +0200

    Complete Go `scrabble-score`

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,30 @@
 package scrabblescore
 
 func Score(word string) int {
-	panic("Please implement the Score function")
+	score := 0
+	for i := range len(word) {
+		char := word[i]
+
+		if char >= 'A' && char <= 'Z' {
+			char += 'a' - 'A'
+		}
+
+		switch char {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score += 1
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		}
+	}
+	return score
 }
