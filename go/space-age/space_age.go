c29c0d8923f7cf97200d14aab397d1ab2f0314a9
Nilirad <gisquerin@gmail.com>
2026-07-08 16:03:33 +0200

    Complete Go `space-age`

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -3,5 +3,27 @@ package spaceage
 type Planet string
 
 func Age(seconds float64, planet Planet) float64 {
-	panic("Please implement the Age function")
+	const secondsInEarthYear float64 = 60 * 60 * 24 * 365.25
+	earthYears := seconds / secondsInEarthYear
+
+	switch planet {
+	case "Mercury":
+		return earthYears / 0.2408467
+	case "Venus":
+		return earthYears / 0.61519726
+	case "Earth":
+		return earthYears
+	case "Mars":
+		return earthYears / 1.8808158
+	case "Jupiter":
+		return earthYears / 11.862615
+	case "Saturn":
+		return earthYears / 29.447498
+	case "Uranus":
+		return earthYears / 84.016846
+	case "Neptune":
+		return earthYears / 164.79132
+	default:
+		return -1
+	}
 }
