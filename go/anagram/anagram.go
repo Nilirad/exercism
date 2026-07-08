ae8e55acb43fb3321574b578802855b2aa2c1e7e
Nilirad <gisquerin@gmail.com>
2026-07-08 17:38:04 +0200

    Complete Go `anagram`

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,5 +1,35 @@
 package anagram
 
+import (
+	"maps"
+	"strings"
+)
+
+func getComposition(s string) map[rune]int {
+	composition := make(map[rune]int)
+	for _, r := range s {
+		composition[r]++
+	}
+
+	return composition
+}
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	subjectLower := strings.ToLower(subject)
+	subjectComposition := getComposition(subjectLower)
+
+	var anagrams []string
+	for _, candidate := range candidates {
+		candidateLower := strings.ToLower(candidate)
+		if subjectLower == candidateLower {
+			continue
+		}
+
+		candidateComposition := getComposition(candidateLower)
+		if maps.Equal(subjectComposition, candidateComposition) {
+			anagrams = append(anagrams, candidate)
+		}
+	}
+
+	return anagrams
 }
