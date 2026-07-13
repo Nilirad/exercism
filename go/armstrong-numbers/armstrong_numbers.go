b7b79d3ae59b0a45c5764fa4b5619ee80984a4e2
Nilirad <gisquerin@gmail.com>
2026-07-13 16:06:56 +0200

    Complete Go `armstrong-numbers`

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,15 @@
 package armstrongnumbers
 
+import "math"
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	digits := int(math.Floor(math.Log10(float64(n)))) + 1
+
+	sum := 0
+	for i := range digits {
+		digit := int(math.Floor(float64(n)/math.Pow10(i))) % 10
+		sum += int(math.Pow(float64(digit), float64(digits)))
+	}
+
+	return sum == n
 }
