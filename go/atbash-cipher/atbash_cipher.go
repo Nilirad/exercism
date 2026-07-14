c542af50510b0aa31d316fee78ff513218cad767
Nilirad <gisquerin@gmail.com>
2026-07-14 18:09:50 +0200

    Complete Go `atbash-cipher`

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,5 +1,35 @@
 package atbashcipher
 
+import "strings"
+
+const chunkSize = 5
+
 func Atbash(s string) string {
-	panic("Please implement the Atbash function")
+	var builder strings.Builder
+	builder.Grow(len(s) + (len(s)+chunkSize-1)/chunkSize + 1)
+
+	counter := 0
+	for i := range len(s) {
+		char := s[i]
+		if char >= 'A' && char <= 'Z' {
+			char += 'a' - 'A'
+		}
+
+		switch {
+		case char >= 'a' && char <= 'z':
+			char = 'a' + 'z' - char
+		case char >= '0' && char <= '9':
+		default:
+			continue
+		}
+
+		if counter >= chunkSize {
+			builder.WriteByte(' ')
+			counter = 0
+		}
+		builder.WriteByte(char)
+		counter++
+	}
+
+	return builder.String()
 }
