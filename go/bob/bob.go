974f828e8b8b31f51de6a22a85c2371dd33df8dc
Nilirad <gisquerin@gmail.com>
2026-07-16 16:13:10 +0200

    Complete Go `bob`

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,24 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package bob
 
-// Hey should have a comment documenting it.
+import "regexp"
+
+var reQuestion = regexp.MustCompile(`\?\s*$`)
+var reYell = regexp.MustCompile(`^[^a-z]*[A-Z]+[^a-z?]*$`)
+var reYellQuestion = regexp.MustCompile(`^[^a-z]*[A-Z]+[^a-z]*\?\s*$`)
+var reSilence = regexp.MustCompile(`^\s*$`)
+
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+
+	switch {
+	case reYellQuestion.MatchString(remark):
+		return "Calm down, I know what I'm doing!"
+	case reQuestion.MatchString(remark):
+		return "Sure."
+	case reYell.MatchString(remark):
+		return "Whoa, chill out!"
+	case reSilence.MatchString(remark):
+		return "Fine. Be that way!"
+	default:
+		return "Whatever."
+	}
 }
