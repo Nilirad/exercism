327ec13cd240140fdec75d50c580227bffcebac4
Nilirad <gisquerin@gmail.com>
2026-07-20 17:35:12 +0200

    Optimize Go `eliuds-eggs`

--- go/eliuds-eggs/eliuds_eggs.go
+++ go/eliuds-eggs/eliuds_eggs.go
@@ -3,13 +3,10 @@ package eliudseggs
 const mask = 1
 
 func EggCount(displayValue int) int {
-	unsignedValue := uint(displayValue)
 	eggs := 0
-	for unsignedValue != 0 {
-		if unsignedValue&mask == 1 {
-			eggs++
-		}
-		unsignedValue >>= 1
+	for displayValue != 0 {
+		displayValue = displayValue & (displayValue - 1)
+		eggs++
 	}
 
 	return eggs
