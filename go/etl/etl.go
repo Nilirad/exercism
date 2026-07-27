a9f5fe7cf662f1168f23c5696f3c0d4534083f48
Nilirad <gisquerin@gmail.com>
2026-07-27 17:37:24 +0200

    Complete Go `etl`

--- go/etl/etl.go
+++ go/etl/etl.go
@@ -1,5 +1,13 @@
 package etl
 
 func Transform(in map[int][]string) map[string]int {
-	panic("Please implement the Transform function")
+	out := make(map[string]int, 26)
+
+	for score, letters := range in {
+		for _, letter := range letters {
+			out[string(letter[0]+'a'-'A')] = score
+		}
+	}
+
+	return out
 }
