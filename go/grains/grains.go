a28f88758d0d0d29cbb379350bf0e33d05fe93bb
Nilirad <gisquerin@gmail.com>
2026-07-28 16:31:05 +0200

    Complete Go `grains`

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,25 @@
 package grains
 
+import (
+	"fmt"
+	"math"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number < 1 || number > 64 {
+		return 0, fmt.Errorf("%d is not a chessboard square", number)
+	}
+
+	// Each bit in an integer corresponds a power of two;
+	// therefore, an integer with all `0`s and one `1`
+	// corresponds to a specific square of Sissa's Chessboard.
+	return uint64(1) << (number - 1), nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	// We have 64 squares, and 64 bits in the number.
+	// Each bit of an integer
+	// corresponds to a specific square of Sissa's Chessboard.
+	// Setting all bits to `1` gives the total grain count.
+	return math.MaxUint64
 }
