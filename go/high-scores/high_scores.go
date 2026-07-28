964d60601d485a89c113acc03fe67bc370a4cebd
Nilirad <gisquerin@gmail.com>
2026-07-28 17:23:33 +0200

    Complete Go `high-scores`

--- go/high-scores/high_scores.go
+++ go/high-scores/high_scores.go
@@ -1,28 +1,67 @@
 package highscores
 
-type HighScores struct{}
+import "math"
+
+type HighScores struct {
+	scores []int
+}
 
 // NewHighScores returns a new HighScores object.
 func NewHighScores(scores []int) *HighScores {
-	panic("Please implement the NewHighScores function")
+	return &HighScores{scores: scores}
 }
 
 // Scores returns all the scores.
 func (s *HighScores) Scores() []int {
-	panic("Please implement the Scores function")
+	return s.scores
 }
 
 // Latest returns the latest (last) score.
 func (s *HighScores) Latest() int {
-	panic("Please implement the Latest function")
+	if len(s.scores) < 1 {
+		return 0
+	}
+
+	return s.scores[len(s.scores)-1]
 }
 
 // PersonalBest returns the best (highest) score.
 func (s *HighScores) PersonalBest() int {
-	panic("Please implement the PersonalBest function")
+	highest := math.MinInt
+
+	for _, score := range s.scores {
+		highest = max(highest, score)
+	}
+
+	return highest
 }
 
 // TopThree returns the top three scores.
 func (s *HighScores) TopThree() []int {
-	panic("Please implement the TopThree function")
+	picked := make([]bool, len(s.scores))
+	topThree := make([]int, 0, 3)
+
+	for range 3 {
+		pickedIndex := -1
+		highest := math.MinInt
+		for i, score := range s.scores {
+			if picked[i] {
+				continue
+			}
+
+			if score >= highest {
+				highest = score
+				pickedIndex = i
+			}
+		}
+
+		if pickedIndex == -1 {
+			break
+		}
+
+		picked[pickedIndex] = true
+		topThree = append(topThree, highest)
+	}
+
+	return topThree
 }
